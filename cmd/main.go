cfa81dd58a51655cfa13a3d13c19d165d456e8a4
Ben Kogan <benkogan9@gmail.com>
2024-02-18 04:43:59 -0500

    make

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,13 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"log/slog"
 	"os"
 )
 
 // Usage: echo <input_text> | your_grep.sh -E <pattern>
 func main() {
 	if len(os.Args) < 3 || os.Args[1] != "-E" {
-		slog.Error("usage: mygrep -E <pattern>")
+		fmt.Println("usage: mygrep -E <pattern>")
 		os.Exit(2) // 1 means no lines were selected, >1 means error
 	}
 
@@ -19,7 +18,7 @@ func main() {
 
 	ok, err := executePattern(os.Stdin, pattern)
 	if err != nil {
-		slog.Error("failed to execute pattern on input", err)
+		fmt.Println("failed to execute pattern on input", err)
 		os.Exit(2)
 	}
 
